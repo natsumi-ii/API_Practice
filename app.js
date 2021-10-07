d0891becec6faeed1a33b5e8fa4d85e46a422457
keisuke ohashi <kog71903@gmail.com>
2021-10-07 23:02:20 +0900

    assignment

--- app.js
+++ app.js
@@ -33,20 +33,6 @@ app.get("/api/v1/search", (req, res) => {
   }
 });
 
-app.get("/api/v1/columns/1", (req, res) => {
-  // columnsの1番目を返す
-  try {
-    const jsonString = JSON.parse(fs.readFileSync("./db.json"));
-    const columns = jsonString.columns;
-
-    res.send(columns[0]);
-    return;
-  } catch (err) {
-    console.log(err);
-    return;
-  }
-});
-
 app.get("/api/v1/columns/count", (req, res) => {
   // columnsの個数を返す
   try {
@@ -76,6 +62,34 @@ app.get("/api/v1/cards/text/count", (req, res) => {
     return;
   }
 });
+
+app.get('/users/:userId/books/:bookId', function (req, res) {
+  const userId = req.params.userId
+  const users = ['first', 'second', 'third']
+  res.send(users[parseInt(userId)])
+})
+
+// TODO: columnsの中から、indexを指定して、合致するcolumnを返せるように変形する
+app.get("/api/v1/columns/1", (req, res) => {
+  // columnsの1番目を返す
+  try {
+    const jsonString = JSON.parse(fs.readFileSync("./db.json"));
+    const columns = jsonString.columns;
+
+    res.send(columns[0]);
+    return;
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+});
+
+// TODO: cardsの中から、indexを指定して、合致するcardを返せるように変形する
+// ここにかく
+
+
+// TODO: cardsOrder の中から、keyを指定して、合致するcardOrderのvalueを返せるように変形する
+// ここにかく
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
